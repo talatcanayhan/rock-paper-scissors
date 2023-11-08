a0501f2f171dff737ffba99716360a9eb9e5d991
talat <talatcan.ayhan@gmail.com>
2023-11-08 09:45:01 +0300

    Added logic but no CSS yet. Will return for more styling.

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,53 +1,62 @@
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 
+//Getting buttons from DOM//
+const rockbtn = document.getElementById("rockButton");
+const paperbtn = document.getElementById('paperButton');
+const scissorsbtn = document.getElementById('scissorsButton');
+
+var computerScore = 0;
+var playerScore = 0;
+
 function getComputerChoice () {
     var randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 
-function getPlayerChoice () {
-    var isChoiceInvalid = true;
-    while (isChoiceInvalid) {
-        playerSelection = prompt();
-        if (choices.includes(playerSelection.toUpperCase())) {
-            isChoiceInvalid = false;
-            return playerSelection.toUpperCase();
-        }
-        else
-            window.alert("Invalid choice. Enter again");       
+function buttonClick(playerChoice) {
+    playRound(playerChoice, getComputerChoice());
+}
+rockbtn.onclick = () => {buttonClick(choices[0]);};
+paperbtn.onclick = () => {buttonClick(choices[1]);};
+scissorsbtn.onclick = () => {buttonClick(choices[2]);};
+
+
+function writeResult (playerSelection, computerSelection, roundWinner, endResult) {
+    endResult = getResult();
+    let result = document.createElement("p");
+
+    if (endResult == "none") {
+        result.innerHTML = "Player: " + playerSelection + "Computer: " + computerSelection + " " + roundWinner;
+        document.getElementById("results").appendChild(result);
     }
+    else {
+        result.innerHTML = endResult;
+        document.getElementById("results").appendChild(result);
+    }
+}
+
+function getResult() {
+    if (playerScore == 5)
+        return "player wins the game";
+    else if (computerScore == 5)
+        return "computer wins the game";
+    else
+        return "none";
 }
 
 function playRound (playerSelection, computerSelection) {
-    if (playerSelection == computerSelection)
-        return "draw";
+    if (playerSelection == computerSelection){
+        writeResult(playerSelection, computerSelection, "draw");
+    }
     else if (
         (playerSelection == choices[0] && computerSelection == choices[1]) ||
         (playerSelection == choices[1] && computerSelection == choices[2]) ||
-        (playerSelection == choices[2] && computerSelection == choices[0]))
-        return "Computer wins";
-    else {
-        return "Player wins";}
-}
-
-function game() {
-    var numOfRounds = 5;
-    var playerScore = 0;
-    var computerScore  = 0;
-    while (numOfRounds != 0) {
-        var result = playRound(getPlayerChoice(),getComputerChoice());
-        if (result == "Player wins")
-            playerScore++;
-        else if (result == "Computer wins")
+        (playerSelection == choices[2] && computerSelection == choices[0])) {
             computerScore++;
-
-        console.log(result);
-        numOfRounds--;
+            writeResult(playerSelection, computerSelection, "Computer wins this round");
+        }
+    else {
+        playerScore++;
+        writeResult(playerSelection, computerSelection, "Player wins this round");
     }
-
-    console.log("Player score: " + playerScore + "\n"
-                + "Computer score: " + computerScore 
-    );
 }
-
-game();
